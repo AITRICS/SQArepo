8043be4f6aecb56a8ec6370a62a5252a18e22a51
Gippeum96 <ppeum@aitrics.com>
2026-03-03 15:57:38 +0900

    deleted:   "VC/ui-tests/P1_tests/playwright_project

--- VC/ui-tests/P1_tests/playwright_project/playwright/fixture/logout.ts
+++
@@ -1,8 +0,0 @@
-import type { Page } from '@playwright/test';
-
-export async function logout(page: Page, loginuser:string) {
-    await page.getByRole('button', { name: `${loginuser} dropdown-arrow` }).click();
-    await page.getByRole('dialog').getByText('로그아웃').click();
-    await page.waitForTimeout(1000);
-  }
-  
